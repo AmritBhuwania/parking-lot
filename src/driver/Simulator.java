5e5dbbf06925156781b68ed398ec094dbe8bf988
amrit <amritkumar.b@perfios.com>
2021-10-23 23:11:48 +0530

    Driver class added

---
+++ src/driver/Simulator.java
@@ -0,0 +1,128 @@
+package driver;
+
+import java.util.List;
+import java.util.Scanner;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+
+import exceptions.ParkingLotException;
+import services.impl.ServicesOfferedImpl;
+
+public class Simulator {
+
+	private static Logger logger = Logger.getLogger(Simulator.class);
+
+	public static void main(String[] args) {
+
+		BasicConfigurator.configure();
+
+		Scanner sc = new Scanner(System.in);
+
+		ServicesOfferedImpl offeredImpl = ServicesOfferedImpl.getInstance();
+
+		logInfoMessage("Enter a value: ");
+		int userInput = sc.nextInt();
+
+		while (userInput != -1) {
+
+			logInfoMessage("Choose an option: ");
+			userInput = sc.nextInt();
+
+			switch (userInput) {
+
+			// Create parking lot of given size
+			case 1:
+
+				logInfoMessage("Enter the maximum car pool size: ");
+				String maxParkingLotSize = sc.next();
+
+				try {
+					offeredImpl.createParkingLot(maxParkingLotSize);
+				} catch (ParkingLotException e) {
+					logger.error(e.getMessage() + ", Exception: " + e);
+				}
+				break;
+
+			case 2:
+
+				logInfoMessage("Car is getting ready to get parked. Enter Registration number and color details of car.");
+				logInfoMessage("Enter the registration number of car: ");
+				String regNo = sc.next();
+				logInfoMessage("Enter the color of car: ");
+				String color = sc.next();
+
+				boolean isCarParkedSuccess = offeredImpl.parkCar(regNo, color);
+				logInfoMessage(String.format("Is car successfully parked: %s", isCarParkedSuccess));
+				break;
+
+			case 3:
+				logInfoMessage("Enter the registration number of car to vacate: ");
+				String regNum = sc.next();
+
+				boolean isCarVacatedSuccess = offeredImpl.leaveParkingLot(regNum);
+				logInfoMessage(String.format("Is car successfully vacated: %s", isCarVacatedSuccess));
+				break;
+
+			case 4:
+				logInfoMessage("Enter the color to get all car registration numbers with provided color: ");
+				color = sc.next();
+
+				List<String> regNums = offeredImpl.getRegNumsFromColor(color);
+
+				if (null != regNums) {
+					logInfoMessage(String.format("Car registration numbers with color %s are: %s",
+							color, regNums));
+				} else {
+					logInfoMessage(String.format("No cars are parked with color %s", color));
+				}
+
+				break;
+
+			case 5:
+				logInfoMessage("Enter the registration number of car to get ticket number: ");
+				regNum = sc.next();
+
+				Integer ticketNum = offeredImpl.getTicketOfRegisteredCar(regNum);
+
+				if (null != ticketNum) {
+					logInfoMessage(String.format("Car ticket number with registration number %s is: %s",
+							regNum, offeredImpl.getTicketOfRegisteredCar(regNum)));
+				} else {
+					logInfoMessage(String.format("No cars is yet parked with registration num as: %s", regNum));
+				}
+
+				break;
+
+			case 6:
+				logInfoMessage("Enter the color to get all car ticket numbers with provided color: ");
+				color = sc.next();
+
+				List<Integer> ticketNums = offeredImpl.getAllTicketsPerColor(color);
+
+				if (null != ticketNums) {
+					logInfoMessage(String.format("Car ticket numbers with color: %s are %s",
+							color, ticketNums));
+				} else {
+					logInfoMessage(String.format("No cars are parked with color: %s", color));
+				}
+
+				break;
+
+			case -1:
+				logInfoMessage("Exiting now!");
+				System.exit(0);
+
+			default:
+				logInfoMessage("Invalid input, please try again with a valid input!");
+				break;
+			}
+
+		}
+	}
+
+	private static void logInfoMessage(String message) {
+		logger.info(String.format("%s%s%s",Thread.currentThread().getName(), "\t", message));
+	}
+
+}
